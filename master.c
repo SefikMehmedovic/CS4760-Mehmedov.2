1dfe70c01d9e94fa799beb65443c99c6c3686faa
sefik mehmedovic <mehmedov@hoare7.cs.umsl.edu>
2018-09-25 22:24:25 -0500

    save before sharememory messes it up

--- master.c
+++ master.c
@@ -2,8 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(int argc, char* argv[]) {
-	printf("test");
-return 0;
+	int input;
+
+	while ((input = getopt (argc, argv, "hn:s:")) != -1)
+	
+	{
+	 switch(input)
+	 {
+		case 'h':
+		printf("To run the program use ./master -n x to indicate the max total of child process to fork off\n");
+		printf("another option is -s to indicate how many children should run at the same time\n ");
+		printf("Example: ./master -n 4 -s 10\n ");
+		break;
+		
+		case 'n':
+		printf("case n\n");
+		break;
+		
+		case 's':
+		printf(" case s ");
+		break;
+	 }
+	
+	}
+	return 0;
 }
 
